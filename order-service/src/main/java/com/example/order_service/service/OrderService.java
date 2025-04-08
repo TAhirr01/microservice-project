c93c7562e2398ce5062f0423ffac1bcd161a669b
Tahir <tahirqasimov001@gmail.com>
2025-04-08 12:51:50 +0400

    Discovery server Neftlix Eureka

--- order-service/src/main/java/com/example/order_service/service/OrderService.java
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order=new Order();
@@ -35,8 +35,8 @@ public class OrderService {
          List<String> skuCodes=order.getOrderLineItems().stream().map(OrderLineItems::getSkuCode).toList();
 
         //Invokes inventory service and if product is in stock place order
-         InventoryResponse[] inventoryResponses=webClient.get()
-                                 .uri("http://localhost:8082/api/inventory",
+         InventoryResponse[] inventoryResponses=webClientBuilder.build().get()
+                                 .uri("http://inventory-service/api/inventory",
                                          uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
                                  .retrieve()
                                  .bodyToMono(InventoryResponse[].class)
--- product-service/src/test/java/com/example/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/example/product_service/ProductServiceApplicationTests.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
