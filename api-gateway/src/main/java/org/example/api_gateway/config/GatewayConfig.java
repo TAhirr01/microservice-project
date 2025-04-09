97fe3c85c804f11d4870b599a75edcc012075919
Tahir <tahirqasimov001@gmail.com>
2025-04-09 17:42:41 +0400

    api gateway product,order,discovery server routes

---
+++ api-gateway/src/main/java/org/example/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,30 @@
+package org.example.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customeRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("product-service",r->r
+                        .path("/api/product")
+                        .uri("lb://product-service"))
+                .route("order-service",r->r
+                        .path("/api/order")
+                        .uri("lb://order-service"))
+                .route("discovery-server",r->r
+                        .path("/eureka/web")
+                        .filters(f->f
+                                .setPath("/")
+                        )
+                        .uri("http://localhost:8761"))
+                .route("discovery-server-static",r->r
+                        .path("/eureka/**")
+                        .uri("http://localhost:8761"))
+                .build();
+    }
+}
