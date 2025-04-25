662044d413ef2ba1b4d4033d01add74536daa378
Tahir <tahirqasimov001@gmail.com>
2025-04-25 22:22:26 +0400

    Distribtued tracing

--- order-service/src/main/java/com/example/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -4,10 +4,14 @@ import com.example.order_service.dto.OrderRequest;
 import com.example.order_service.model.Order;
 import com.example.order_service.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -17,15 +21,18 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    //burdaki name ile app.properties-de ki .instances-den soraki eyni olmalidir
     @CircuitBreaker(name = "inventory",fallbackMethod = "fallbackMethod")
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
-         orderService.placeOrder(orderRequest);
-         log.info("Order placed successfully");
-         return "Order placed successfully";
+    @TimeLimiter(name="inventory")
+
+    //retry implement eliyenden 500 verir
+    @Retry(name="inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest){
+       return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest));
     }
 
-    public String fallbackMethod(OrderRequest orderRequest, RuntimeException exception){
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException exception){
         log.info( "Something went wrong, come back after some time");
-        return "Something went wrong, come back after some time";
+        return CompletableFuture.supplyAsync(()->"Something went wrong, come back after some time");
     }
 }
--- order-service/src/main/java/com/example/order_service/service/OrderService.java
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -6,11 +6,15 @@ import com.example.order_service.mapper.OrderLineItemsMapper;
 import com.example.order_service.model.Order;
 import com.example.order_service.model.OrderLineItems;
 import com.example.order_service.repository.OrderRepository;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.xml.parsers.SAXParser;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -18,12 +22,14 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public String placeOrder(OrderRequest orderRequest){
         Order order=new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -34,21 +40,32 @@ public class OrderService {
 
          List<String> skuCodes=order.getOrderLineItems().stream().map(OrderLineItems::getSkuCode).toList();
 
-        //Invokes inventory service and if product is in stock place order
-         InventoryResponse[] inventoryResponses=webClientBuilder.build().get()
-                                 .uri("http://inventory-service/api/inventory",
-                                         uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
-                                 .retrieve()
-                                 .bodyToMono(InventoryResponse[].class)
-                                 .block();
+         log.info("Calling inventory service");
+
+         Span inventoryServiceLookup=tracer.nextSpan().name("InventoryServiceLookup");
+
+         try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())){
+
+             //Invokes inventory service and if product is in stock place order
+             InventoryResponse[] inventoryResponses=webClientBuilder.build().get()
+                     .uri("http://inventory-service/api/inventory",
+                             uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                     .retrieve()
+                     .bodyToMono(InventoryResponse[].class)
+                     .block();
 
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
+             boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
 
-         if (allProductsInStock){
-             orderRepository.save(order);
-         }else{
-             throw new IllegalArgumentException("No product in stock");
+             if (allProductsInStock){
+                 orderRepository.save(order);
+                 return "Order placed successfully";
+             }else{
+                 throw new IllegalArgumentException("No product in stock");
+             }
+
+         }finally {
+             inventoryServiceLookup.end();
          }
 
     }
